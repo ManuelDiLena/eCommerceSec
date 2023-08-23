74810ebc55ad271f5eb5247a982c4b0b55f1537a
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-08-23 19:40:48 +0200

    Images of the items

--- pages/store/index.js
+++ pages/store/index.js
@@ -1,12 +1,15 @@
 import Layout from '../../components/Layout'
-import { getItems } from '../../services/storeService';
+import { getItems } from '../../services/storeService'
+import Image from 'next/image'
 
 export default function Index({items}) {
     return (
         <Layout>
             <h1>Store</h1>
             {
-                items && items.map(item => <div key={item.id}>{item.title}</div>)
+                items && items.map(item => <div key={item.id}>
+                    <Image src={item.image} alt='' width='200' height={200} />
+                    {item.title}</div>)
             }
         </Layout>
     );
